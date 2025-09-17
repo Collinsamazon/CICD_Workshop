26616c92ef9937343914d68c3d1c18498ee678aa
collinsamazon <nwanzecc@amazon.com>
2025-09-17 08:17:21 +0000

    Initial Commit

---
+++ app-cdk/bin/app-cdk.ts
@@ -0,0 +1,8 @@
+import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
+import * as cdk from 'aws-cdk-lib';
+import { AppCdkStack } from '../lib/app-cdk-stack';
+import 'source-map-support/register';
+
+const app = new cdk.App();
+const testCdkStack = new AppCdkStack(app, 'test', {});
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {});
---
+++ my-app/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+/// <reference types='vite/client' />
+  base: '/my-app',
+  server: {
+    allowedHosts: true,
+    host: true,
+    port: 8081,
+  },
+})
